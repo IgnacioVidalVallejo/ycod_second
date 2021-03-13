4f1bc4883fa6a783235136cdf13d18fe7a5dd228
Ignacio Vidal Vallejo <ignaciovidalvallejo@gmail.com>
2021-03-13 13:31:52 +0100

    DONE ** vue link by CDN - tailwindcss link by CDN (don t allow theme customization, demanded color on style directive on tag

--- resources/views/people.blade.php
+++ resources/views/people.blade.php
@@ -7,6 +7,8 @@
         <div id="app">
             <people-component></people-component>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
+        <script src="https://widget.cloudinary.com/v2.0/global/all.js" type="text/javascript"></script>
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
